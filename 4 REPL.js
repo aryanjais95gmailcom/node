fc0c73a66c38cf26597fb9fc53ca80ee69f020f7
Aryan Choudhary <aryanjais95@gmail.com>
2024-10-12 15:09:51 +0530

    Add files via upload

--- 4 REPL.js
+++ 4 REPL.js
@@ -1,3 +1,19 @@
+//REPl
+// 1: js Expresssion
+// 2: use variable
+// 3: Multiline code
+// 4: use (_) to get the last result
+// 5: we cabe editor mode
+
 // The REPL features of Node is very useful in experimenting with Node.js codes and to debug JavaScript codes.
-// REPl is just nodeJs cmd
-     //Read - Reads users's input,parses the input into JavaScriput data-struture,and stores
+/* REPl is just nodeJs cmd
+     Read - Reads users's input,parses the input into JavaScriput data-struture,and stores in memory.
+     Eval - takes and evaluates the data struture.
+     Print- Print and result
+     Loop- Loops the above command until the suser presses ctrl-c twice.*/
+/*To create a new file through terminal type 
+     type nul > filename.js*/
+     
+//.editor start the editor mode in terminal
+// double click on tab to see porperty 
+// fs  to see file system
